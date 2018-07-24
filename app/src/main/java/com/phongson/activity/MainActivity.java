159f05ea493b2e85e4d741e781b091845f66d098
Phong <tvqphong001@github.com>
2018-07-24 10:24:04 +0700

    a

--- app/src/main/java/com/phongson/activity/MainActivity.java
+++ app/src/main/java/com/phongson/activity/MainActivity.java
@@ -1,11 +1,16 @@
 package com.phongson.activity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -17,8 +22,11 @@ import com.phongson.model.ChuyenMuc;
 import com.phongson.model.LinkTinTuc;
 import com.phongson.model.NguonTinTuc;
 import com.phongson.model.TinDaLuu;
+import com.phongson.util.NetworkStateChangeReceiver;
 
 import java.util.ArrayList;
+
+import static com.phongson.util.NetworkStateChangeReceiver.IS_NETWORK_AVAILABLE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,18 +39,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getDataChuyenMuc();
-        getdataNguonTinTuc();
-        getdataLinkTinTuc();
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(MainActivity.this,Main2Activity.class));
-            }
-        },5000);
+        if (checkNetwork()) {
+            getDataChuyenMuc();
+            getdataNguonTinTuc();
+            getdataLinkTinTuc();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(MainActivity.this, Main2Activity.class));
+                }
+            }, 5000);
 
+        }
+        else Toast.makeText(this, "Vui Long Kiem Tra Internet!", Toast.LENGTH_SHORT).show();
 
     }
+
+    private boolean checkNetwork() {
+        NetworkStateChangeReceiver networkStateChangeReceiver = new NetworkStateChangeReceiver();
+        boolean internet = networkStateChangeReceiver.isConnectedToInternet(MainActivity.this);
+        return internet;
+    }
+
 
     private void getdataLinkTinTuc() {
         listLinkTinTuc = new ArrayList<>();
